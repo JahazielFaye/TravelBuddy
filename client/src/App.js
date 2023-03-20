d044d74ae39a75b4230a677c3179e67d65a98573
Cristina Geremia <CG@Cristinas-Air.lan>
2023-03-20 18:36:44 -0400

    fix app.js file to make into single page

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route, NavLink, HashRouter } from 'react-router-dom'
+import Home from "./Home/home.jsx"
+import Trips from "../Trips/trips.jsx"
+import Activities from "../"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <HashRouter>
+        <div>
+          <h1>Travel Buddy</h1>
+          <ul className="header">
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/trips">Trips</NavLink>
+            </li>
+            <li>
+              <NavLink to="/activities">Activities</NavLink>
+            </li>
+          </ul>
+          <div className="content">
+            <Route path="/" component={Home} />
+            <Route path="/trips" component={Trips} />
+            <Route path="/activities" component={Activities} />
+          </div>
+        </div>
+      </HashRouter>
+    );
+  }
 }
-
 export default App;
